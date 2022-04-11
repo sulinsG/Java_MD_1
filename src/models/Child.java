73cf52cb0bff0ce0c3ac2143d8b15294b12fc17f
Gatis <gatis.sulins@gmail.com>
2022-04-11 13:04:46 +0300

    finished Child class

--- src/models/Child.java
+++ src/models/Child.java
@@ -1,10 +1,67 @@
 package models;
 
-public class Child {
+public class Child extends Person {
     private String allergies;
     private int priorityForSpeechLessons;
     private Nationality nationality;
 
+
+    public Nationality getNationality() {
+        return this.nationality;
+    }
+
+    public void setNationality(Nationality nationality) {
+        this.nationality = nationality;
+    }
+
+
+    public String getAllergies() {
+        return this.allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        if(!allergies.isEmpty() && allergies.matches("[A-ZĀČĒĢĪĶĻŅŠŪŽ]{1}[a-zāčēģīķļņšūž]+"))
+            this.allergies = allergies;
+        else
+        this.allergies = "Unknown";
+    }
+
+    public int getPriorityForSpeechLessons() {
+        return this.priorityForSpeechLessons;
+    }
+
+    public void setPriorityForSpeechLessons(int priorityForSpeechLessons) {
+        if(priorityForSpeechLessons >= 1 && priorityForSpeechLessons <= 6)
+        this.priorityForSpeechLessons = priorityForSpeechLessons;
+        else
+        this.priorityForSpeechLessons = 0;
+    }
+
+
+    public Child() {
+        super();
+        setNationality(Nationality.OTHER);
+        setAllergies("Unknown");
+        setPriorityForSpeechLessons(0);
+    }
+
+    public Child(String name, String surname, String personalCode, String allergies, int priorityForSpeechLessons, Nationality nationality) {
+
+        super(name,surname,personalCode);
+        setNationality(nationality);
+        setAllergies(allergies);
+        setPriorityForSpeechLessons(priorityForSpeechLessons);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " allergies='" + getAllergies() + "'" +
+            ", priorityForSpeechLessons='" + getPriorityForSpeechLessons() + "'" +
+            "}";
+    }
+
+    
 
     
 }
